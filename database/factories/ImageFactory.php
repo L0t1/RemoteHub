b241f8e4b61c9468a6670092735df284bf0f83a7
Lot Kyçyku <lotkyqyku@gmail.com>
2023-01-17 13:41:30 +0100

    factories

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -2,22 +2,27 @@
 
 namespace Database\Factories;
 
+use App\Models\Image;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
- */
 class ImageFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Image::class;
+
+    /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
     public function definition()
     {
         return [
-            //
+            'path' => 'image.png'
         ];
     }
 }
--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -2,22 +2,34 @@
 
 namespace Database\Factories;
 
+use App\Models\Office;
+use App\Models\Reservation;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reservation>
- */
 class ReservationFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Reservation::class;
+
+    /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
-    public function definition()
+    public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'office_id' => Office::factory(),
+            'price' => $this->faker->numberBetween(10_000, 20_000),
+            'status' => 1,
+            'start_date' => now()->addDay()->format('Y-m-d'),
+            'end_date' => now()->addDays(5)->format('Y-m-d'),
         ];
     }
 }
