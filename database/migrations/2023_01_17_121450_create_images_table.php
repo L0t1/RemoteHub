4502d562ae056d3bfcfd6a4dfa1ec348b738d50f
Lot Kyçyku <lotkyqyku@gmail.com>
2023-01-17 13:33:06 +0100

    migrations

--- database/migrations/2023_01_17_121450_create_images_table.php
+++ database/migrations/2023_01_17_121450_create_images_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
+            $table->morphs('resource');
+            $table->string('path');
             $table->timestamps();
         });
     }
