a5a98830d4880f66a0d0fbe7add8d3e3b6627b1c
Lot Kyçyku <lotkyqyku@gmail.com>
2023-01-18 14:36:19 +0100

    show only lists of offices that are approved and not hidden

--- tests/Feature/OfficeControllerTest.php
+++ tests/Feature/OfficeControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\Office;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use PHPUnit\Framework\Test;
 use Tests\TestCase;
 
 class OfficeControllerTest extends TestCase
@@ -20,14 +21,27 @@ class OfficeControllerTest extends TestCase
         Office::factory(3)->create();
 
         $response = $this->get('/api/offices');
-        $response->dump();
 
         $response->assertOk();
         $response->assertJsonCount(3,'data');
-
         $this->assertNotNull($response->json('data')[0]['id']);
         $this->assertNotNull($response->json('meta'));
         $this->assertNotNull($response->json('links'));
-
     }
+
+    /**
+     * @test
+     */
+
+     public function itOnlyListsOfficesThatAreApprovedAndNotHidden()
+     {
+        Office::factory(3)->create();
+        Office::factory(3)->create(['hidden' => true]);
+        Office::factory(3)->create(['approval_status' => Office::APPROVAL_PENDING]);
+
+        $response = $this->get('/api/offices');
+
+        $response->assertOk();
+        $response->assertJsonCount(3,'data');
+     }
 }
