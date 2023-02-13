77cbb2c47a4277cf5b40db675844fc50badd38d5
Lot Kyçyku <lotkyqyku@gmail.com>
2023-02-13 15:01:33 +0100

    removed office  approval status of rejection.

--- database/factories/OfficeFactory.php
+++ database/factories/OfficeFactory.php
@@ -31,8 +31,22 @@ class OfficeFactory extends Factory
             'address_line1' => $this->faker->address,
             'approval_status' => Office::APPROVAL_APPROVED,
             'hidden' => false,
-            'price_per_day' => $this->faker->randomElement([1_000, 2_000, 3_000, 4_000]),
+            'price_per_day' => $this->faker->numberBetween(1_000, 2_000),
             'monthly_discount' => 0
         ];
+    }
+
+    public function pending(): Factory
+    {
+        return $this->state([
+            'approval_status' => Office::APPROVAL_PENDING,
+        ]);
+    }
+
+    public function hidden(): Factory
+    {
+        return $this->state([
+            'hidden' => true,
+        ]);
     }
 }
